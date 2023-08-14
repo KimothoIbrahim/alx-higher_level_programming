6125c7bb1532cb322649b12f3484a04d21a06360
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-08-14 14:36:19 +0000

    Add second modules task

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """For inputs 10 and 5 print their sum/difference/quotient/multiplication."""
+    """For inputs 10 and 5 print their sum
+	difference quotient multiplication."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
@@ -11,4 +12,3 @@ if __name__ == "__main__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
-
