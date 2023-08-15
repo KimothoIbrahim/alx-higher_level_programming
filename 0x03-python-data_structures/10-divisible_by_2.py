3b8e1beddddc0d6427fe42856f81202cc3b7296c
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-08-15 14:22:12 -0700

    reddish

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 10-divisible_by_2.py
+
+def divisible_by_2(my_list=[]):
+    """MUL"""
+    times = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            times.append(True)
+        else:
+            times.append(False)
+
+        return (times)
