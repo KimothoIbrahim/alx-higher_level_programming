3d251ec8cefd724b2207eee183b5d1b4fb4a56b2
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-08-28 11:03:29 -0700

    Adjust task 3:0x05-python-exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -14,7 +14,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for t in range(0, x):
         try:
             print("{:d}".format(my_list[t]), end="")
-            ret += 1
+            numy += 1
         except (ValueError, TypeError):
             continue
     print("")
