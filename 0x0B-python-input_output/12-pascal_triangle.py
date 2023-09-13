0f186dca801ec15af238bcedde80b13b27c98e01
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-09-12 18:36:04 -0700

    task last

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,7 +11,7 @@ def pascal_triangle(n):
     triA = [[1]]
     while len(triA) != n:
         tri = triA[-1]
-        tmp = [1]
+        temp = [1]
         for i in range(len(tri) - 1):
             temp.append(tri[i] + tri[i + 1])
         temp.append(1)
