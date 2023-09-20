2ce2c47ecd29c8a843a571d9f46983e4b7b6c155
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-09-20 00:27:39 -0700

    Adjust contents

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# 0-print_list_integer.py
 
 def print_list_integer(my_list=[]):
-	"""Text"""
-	for i in range(len(my_list)):
-		print("{:d}".format(my_list[i]))
+    for x in my_list:
+        print("{}".format(x))
