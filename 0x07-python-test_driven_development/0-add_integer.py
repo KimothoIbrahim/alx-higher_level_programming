23e528846c23ccff952f936b63961dab205dd466
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-10-29 07:57:04 -0700

    Resubmit 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,13 +13,13 @@ def add_integer(a, b=98):
     Returns:
         sum of a and b
     """
-    
+
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     elif type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    
+
     a = int(a)
     b = int(b)
-    
+
     return (a+b)
