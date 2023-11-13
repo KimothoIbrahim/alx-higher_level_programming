336c29b53e7fd43c38795f6378dd00cfc7c723a0
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-11-13 13:20:23 -0800

    Push some

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
 if (Number(process.argv[2])) {
-  console.log('My Number: ' + Number(process.argv[2]));
+  console.log('My Number:' + Number(process.argv[2]));
 } else { console.log('Not a number'); }
