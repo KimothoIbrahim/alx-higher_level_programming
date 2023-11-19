ce8aa449398be24b8868fb6d7c4f2b6034309424
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-11-19 13:22:13 +0300

    Add instance checker

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-"""Class-checking function."""
+"""Module does something"""
 
 
 def is_kind_of_class(obj, a_class):
-    """See if instance or inherited instance of some class.
-
-    Args:
-        obj (any): Object to check.
-        a_class (type): The class.
-    Returns:
-        True or False.
-    """
-    if isinstance(obj, a_class):
-        return True
-    return False
+    """check if oobj is an instance of a_class"""
+    return isinstance(obj, a_class)
