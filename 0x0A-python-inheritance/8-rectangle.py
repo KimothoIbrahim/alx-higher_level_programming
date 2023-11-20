fc43abb06b263c1176c6b57b8b6302c64229c956
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-11-20 13:47:07 +0300

    Add an Inheriting class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,7 +8,7 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 class Rectangle(BaseGeometry):
     """inheriting class"""
     def __init__(self, width, height):
-        BaseGeometry.integer_validator(self, "width", width)
-        BaseGeometry.integer_validator(self, "height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.width = width
         self.height = height
