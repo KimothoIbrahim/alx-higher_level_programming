4fc30ddaf00530f553f9d390bfbf953fa394b93a
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-11-22 14:44:30 +0300

    Do task 4

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Working with JSON"""
+"""represent a give object in JSON"""
 import json
 
 
 def to_json_string(my_obj):
-    """Return json representation of an object
+    """return the JSON representation of `my_obj`
 
     Args:
-        my_obj: given object
+        my_obj (object): the object to represent as a JSON string
 
-    Returns:
-        string representstion of object
+    Return:
+        JSON string representaion
     """
     return json.dumps(my_obj)
