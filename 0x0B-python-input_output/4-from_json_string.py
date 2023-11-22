62372389151cafa6733bd938cbf46c7f6e43ddda
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-11-22 15:01:53 +0300

    Do another task

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-"""Working with JSON"""
+"""Deserializes JSON string to an object"""
 import json
 
 
 def from_json_string(my_str):
-    """convert deserialize JSON"""
+    """
+    function converts the JSON string
+    `my_str` back to a python object
+
+    Args:
+        my_str (str): given JSON string
+
+    Return:
+        desrialized object
+    """
     return json.loads(my_str)
