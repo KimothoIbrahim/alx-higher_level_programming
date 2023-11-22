3bf89f2608e6fee1640d8d53937d545d39b0045f
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-11-22 12:18:07 +0300

    Push own edit

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,17 @@
-#!/usr/bin/python3
-"""Writing to files"""
+#!/usr/binpython3
+"""script writes given text to a file"""
 
 
 def write_file(filename="", text=""):
-    """Write to text file
+    """Function write the text `text` to
+    the file `filename`
 
     Args:
-        filename (str): File to be written
-        text (str): text to write in file
-    Returns:
-        number of characters written
+        filename (str): name of file to be written
+        text (str): content to be written
+
+    Return:
+        number of characters written to `filename`
     """
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+    with open(filename, 'w', encoding='utf-8') as file:
+        return (file.write(text))
