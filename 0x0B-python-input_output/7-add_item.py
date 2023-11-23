48974f053d7dedab60a5233669758b7e174a8f30
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-11-23 09:47:45 +0300

    Accomplish aotomation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""save all arguments to a file."""
+"""Script taking args into list and saves to JSON file"""
 import sys
-
-
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file =\
+        __import__("6-load_from_json_file").load_from_json_file
+
+    file = "add_item.json"
+    buffer = []
 
     try:
-        items = load_from_json_file("add_item.json")
+        buffer = load_from_json_file(file)
+
+        for i in sys.argv:
+            if i != sys.argv[0]:
+                buffer.append(i)
     except FileNotFoundError:
-        items = []
-        items.extend(sys.argv[1:])
-        save_to_json_file(items, "add_item.json")
+        buffer = sys.argv[1:]
+
+    save_to_json_file(buffer, file)
