9326c803b706925fab04b94a8ee2bd9c26a35c2c
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-11-23 12:58:40 +0300

    Make own student rep

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""defines a Class"""
 
 
 class Student:
-    """Student representation."""
 
     def __init__(self, first_name, last_name, age):
-        """new Student.
-
-        Args:
-            first_name (str): The student's first name..
-            last_name (str): Their last name.
-            age (int): Student age.
-        """
+        """initialize a Student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Get a dictionary representation of Student."""
         return self.__dict__
