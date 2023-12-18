a0be4e0290a81bb74ffc7f67beb788314f36fa1e
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-12-18 19:55:39 +0300

    Make script printing response body bytte size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # curl the argument provided as host and display body size
-
-if [ $# -eq 1 ]; then
-	curl -s $1 -w "%{size_download}\n" -o /dev/null
-fi
+curl -s $1 -w "%{size_download}\n" -o /dev/null
