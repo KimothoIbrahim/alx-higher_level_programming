6962ec60ff765bdbcf28fb1bf12eb41cf0f7e059
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-12-20 17:01:00 +0300

    Read from url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,16 +3,11 @@
 
 import urllib.request
 
-try:
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        res = response.read()
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    res = response.read()
 
-        print('Body response:')
-        print(' - type: {}'.format(type(res)))
-        print(' - content: {}'.format(res))
-        print(' - utf8 content: {}'.format(res.decode('utf-8')))
-except HTTPError as e:
-    print(e.code)
-except URLError as e:
-    print(e.code)
+    print('Body response:')
+    print(' - type: {}'.format(type(res)))
+    print(' - content: {}'.format(res))
+    print(' - utf8 content: {}'.format(res.decode('utf-8')))
