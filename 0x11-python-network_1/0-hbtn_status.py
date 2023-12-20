1cd37fa5d7f292f2707f1df70ea7ad8d61d6f95f
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-12-20 22:35:40 +0300

    Remake

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ The script fetches a url using urllib module"""
 
-import urllib.request
+if __name__ == "__main__":
+    import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    res = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        res = response.read()
 
-print('Body response:')
-print(' - type: {}'.format(type(res)))
-print(' - content: {}'.format(res))
-print(' - utf8 content: {}'.format(res.decode('utf-8')))
+        print('Body response:')
+        print(' - type: {}'.format(type(res)))
+        print(' - content: {}'.format(res))
+        print(' - utf8 content: {}'.format(res.decode('utf-8')))
