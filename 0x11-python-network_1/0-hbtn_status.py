9987025e7ba0c7c4747d44f4a6eedb57b921c9bd
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-12-20 16:57:26 +0300

    Read from url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,9 +9,9 @@ try:
         res = response.read()
 
         print('Body response:')
-        print(' - type: {} '.format(type(res)))
-        print(' - content: {} '.format(res))
-        print(' - utf8 content: {} '.format(res.decode('utf-8')))
+        print(' - type: {}'.format(type(res)))
+        print(' - content: {}'.format(res))
+        print(' - utf8 content: {}'.format(res.decode('utf-8')))
 except HTTPError as e:
     print(e.code)
 except URLError as e:
