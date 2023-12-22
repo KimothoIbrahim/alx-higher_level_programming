82d5423e44a4876dca1f537b3a308a0687a07eea
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-12-22 20:29:55 +0300

    DO py networks

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" """
+
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    import urllib.parse
+
+    vals = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(vals)
+    data = data.encode('ascii')
+
+    req = urllib.request.Request(sys.argv[1], data)
+
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+        print(page.decode('utf-8'))
