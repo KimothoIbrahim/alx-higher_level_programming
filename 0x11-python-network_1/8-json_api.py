0b46c2f2f018566746812d7e4c75f700874e4ad5
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-12-23 17:44:11 +0300

    Make json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Use json"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    data = {'q': ""}
+
+    if len(sys.argv) > 1:
+        data = {'q': sys.argv[1]}
+
+    r = requests.post('http://0.0.0.0:5000/search_user', data=data)
+
+    if r.ok:
+        try:
+            r = r.json()
+            if r:
+                print('', r.get('id'), r.get('name'))
+            else:
+                print('No result')
+        except ValueError:
+            print('Not a valid JSON')
