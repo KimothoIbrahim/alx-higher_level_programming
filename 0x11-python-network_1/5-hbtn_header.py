d38718d1025c75f7346e3f6372b7fbee78941818
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-12-23 13:22:47 +0300

    Read headers

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Print the response header"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
