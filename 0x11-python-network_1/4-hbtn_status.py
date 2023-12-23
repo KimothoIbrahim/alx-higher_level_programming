97a8c9013c08307910a6bf289a4698aab9e3a0ba
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-12-23 11:48:47 +0300

    Make get request using Requests module

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     r = requests.get('https://alx-intranet.hbtn.io/status')
 
     print('Body response:')
-    print('     - type:', type(r.text))
-    print('     - content:', r.text)
+    print('\t- type:', type(r.text))
+    print('\t- content:', r.text)
