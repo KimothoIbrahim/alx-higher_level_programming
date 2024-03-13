2dfe3748b1f08d3a6c1878be17d1e8a4404a105c
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-03-13 03:16:20 +0300

    Improved algo

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,27 +1,31 @@
 #!/usr/bin/node
 
-const n = process.argv;
+const m = process.argv;
 
-if (process.argv[2]) {
-  if (!process.argv[3]) {
-    console.log('0');
+function secondBiggest (n) {
+  if (n[2]) {
+    if (!n[3]) {
+      console.log('0');
+    } else {
+      let y = Number(n[2]);
+      for (let x = 2; x < n.length; x++) {
+        if (Number(n[x]) > y) {
+          y = Number(n[x]);
+        }
+      }
+
+      let z = Number(n[2]);
+      for (let x = 2; x < n.length; x++) {
+        if (Number(n[x]) > z && Number(n[x]) < y) {
+          z = Number(n[x]);
+        }
+      }
+
+      console.log(z);
+    }
   } else {
-    let y = Number(n[2]);
-    for (let x = 2; x < n.length; x++) {
-      if (Number(n[x]) > y) {
-        y = Number(n[x]);
-      }
-    }
-
-    let z = Number(n[2]);
-    for (let x = 2; x < n.length; x++) {
-      if (Number(n[x]) > z && Number(n[x]) < y) {
-        z = Number(n[x]);
-      }
-    }
-
-    console.log(z);
+    console.log('0');
   }
-} else {
-  console.log('0');
 }
+
+secondBiggest(m);
