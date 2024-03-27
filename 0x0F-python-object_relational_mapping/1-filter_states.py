d6337e238e026d7c66057f9d3233f2225ff357fb
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-03-27 16:56:04 +0300

    Filter select results with LIKE keyword

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states where states.name
+    cur.execute("SELECT * FROM states WHERE states.name
                 LIKE BINARY 'N%' ORDER BY states.id")
     states = cur.fetchall()
     for state in states:
