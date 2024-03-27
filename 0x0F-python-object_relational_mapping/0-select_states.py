4ad5811245c7877da8d8316c6784c10cd1f6da8b
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-03-27 13:07:52 +0300

    Use mysqldb the client to query a database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY states.id;")
+states = cur.fetchall()
+for state in states:
+    print(state)
+cur.close()
+conn.close()
