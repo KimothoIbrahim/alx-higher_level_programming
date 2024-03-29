59a02db6995a9412da42e853ffb2424df4440375
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-03-29 22:22:42 +0300

    Show one state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a script that lists all State objects from the database hbtn_0e_6_usa"""
+
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    import sys
+
+    url = ("mysql+mysqldb://{}:{}@localhost:3306/{}"
+           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine(url)
+
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+
+    if state:
+        print(state.id, state.name, sep=": ")
+    else:
+        print("Nothing")
