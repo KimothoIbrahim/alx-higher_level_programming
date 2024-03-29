8f41a49b5e7b97c8eb53d1c8d9f7a5021ede7757
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-03-29 16:52:59 +0300

    Create states table in sqlAlchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/pyhton3
-"""script defining models/classes/tables for database"""
+"""
+script defining models/classes/tables for database
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import String, Integer, Column, MetaData
 
@@ -8,7 +10,9 @@ Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
-    """ define the states table"""
-    __tablename__ = "states"
+    """
+    define the states table
+    """
+    __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
