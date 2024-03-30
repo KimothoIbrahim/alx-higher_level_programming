c9908158fb74030a1d59098c9bb7e7250ebec6da
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-03-30 20:18:54 +0300

    Craete relations between tables

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""join states and cities tables"""
+
+from model_state import State, Base
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+url = ("mysql+mysqldb://{}:{}@localhost:3306/{}"
+       .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+engine = create_engine(url)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+cities = (session.query(State.name, City.id, City.name)
+          .filter(State.id == City.state_id).all())
+
+for instance in cities:
+    print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,10 @@
+from sqlalchemy import String, Integer, Column, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """declare a city table"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
