bca6810f2b1b3ba1e6ffadc0dd203d315f2b1623
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-04-18 12:27:34 +0300

    Sorting algorith

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def find_peak(list_of_integers):
-  arr = sorted(list_of_integers)
-  if arr:
-    return arr[-1]
+    arr = sorted(list_of_integers)
+    if arr:
+        return arr[-1]
