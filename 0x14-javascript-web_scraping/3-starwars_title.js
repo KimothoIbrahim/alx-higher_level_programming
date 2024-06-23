aeab47244ab32f8d7186bd8eb08285dc68b409e0
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-06-23 18:13:01 +0300

    Get a movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@ const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
 request(url, (error, response, body) => {
-  if (!error && response.statusCode == 200) {
+  if (!error && response.statusCode === 200) {
     const info = JSON.parse(body);
     console.log(info.title);
   }
