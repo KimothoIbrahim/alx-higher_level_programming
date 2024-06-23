e7ec9ec8d5784a7acb7c6d323133c1898e66c55f
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-06-23 07:36:53 +0300

    Reading a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,12 @@
 
 fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
-  if (err) {
-    console.error('The was an error reading the file! :', err);
-    return;
-  }
-  console.log('The supplied file contains the following content:', data);
-});
+try {
+  fs.readFile(process.argv[2], 'utf8', (err, data) => {
+    if (err) {
+      console.error('The was an error reading the file! :', err);
+      return;
+    }
+    console.log('The supplied file contains the following content:', data);
+  });
+} catch (err) { console.log(err); }
