d9b1fd83ee945896db04f092fc39b7d00201fef4
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-06-23 07:40:38 +0300

    Reading a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 fs = require('fs');
 
 try {
-  fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  fs.readFile(process.argv[2], 'utf-8', (err, data) => {
     if (err) {
       console.error('The was an error reading the file! :', err);
       return;
