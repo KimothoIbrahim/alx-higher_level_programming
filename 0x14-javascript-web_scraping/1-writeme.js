445f59529188c010211bca74257f93fedd1cacf4
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-06-23 15:30:27 +0300

    Write to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,5 +3,5 @@
 const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', (error) => {
-  if (!error) { console.log('file Written'); } else { console.log(error); }
+  if (error) { console.log(error); }
 });
