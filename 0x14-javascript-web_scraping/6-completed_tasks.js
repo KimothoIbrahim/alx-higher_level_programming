8426ba07511181e0859d36a147e059fb209d3f97
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-06-24 15:50:43 +0300

    count done tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request'); const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const info = JSON.parse(body).filter((todo) => todo.completed);
+
+    const ob = {};
+
+    for (let i = 1; i < 11; i++) {
+      const count = info.filter((todo) => { return todo.userId == i; });
+      ob[i] = count.length;
+    }
+
+    console.log(ob);
+  }
+});
